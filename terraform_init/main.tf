7abb2b8be0f6370d7a0dd243be2f135696bd629e
Joseph Ligier <littel.jo@gmail.com>
2021-01-25 15:45:02 +0000

    first commit

---
+++ terraform_init/main.tf
@@ -0,0 +1,36 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "kubeone"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.r.id
+}
+
+output vpc_id {
+  value = aws_vpc.main.id
+}
